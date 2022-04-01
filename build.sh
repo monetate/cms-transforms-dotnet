36290e5f55d862926dc9ba6620d2a6612bf38493
Tyler Weaver <Tyler.Weaver@kibocommerce.com>
2022-03-31 21:10:47 -0400

    WIP:

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+yum install python
+yum install awscli
+
 ARTIFACTORY_TOKEN=$(aws s3 cp s3://secret-monetate-dev/artifactory/monetate.jfrog.io/dotnet-local/dotnet-local-pw -)
 echo ${ARTIFACTORY_TOKEN}
