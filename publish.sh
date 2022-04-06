0063b7e78b218e9724dbcc6c72185f951e0f994e
Tyler Weaver <Tyler.Weaver@kibocommerce.com>
2022-04-06 18:25:01 -0400

    tst

--- publish.sh
+++ publish.sh
@@ -2,20 +2,9 @@
 
 VERSION_PREFIX=$(cd cms-transforms-c-sharp/CmsTransformLibrary && grep '<Version>' < CmsTransformLibrary.csproj | sed 's/.*<Version>\(.*\)<\/Version>/\1/')
 VERSION_PREFIX="$(echo "$VERSION_PREFIX" | tr -d '\r\n')"
-ARTIFACTORY_UNAME=$(aws s3 cp s3://secret-monetate-dev/artifactory/monetate.jfrog.io/dotnet-local/dotnet-local-user -)
-ARTIFACTORY_PW=$(aws s3 cp s3://secret-monetate-dev/artifactory/monetate.jfrog.io/dotnet-local/dotnet-local-upw -)
 SOURCE="CmsTransformLibrary.${VERSION_PREFIX}.nupkg"
-API_KEY="${ARTIFACTORY_UNAME}:${ARTIFACTORY_PW}"
 IFS=: read -r USER PW <<< "$1"
-echo "USER"
-echo "$USER"
-echo "${USER}"
-echo "PW"
-echo "$PW"
-echo "${PW}"
-echo "ONE"
-echo "$1"
-echo "${1}"
-dotnet nuget add source https://monetate.jfrog.io/artifactory/api/nuget/v3/dotnet-local -n Artifactory -u ${USER} -p ${PW} --store-password-in-clear-text
 
-cd cms-transforms-c-sharp/CmsTransformLibrary/bin/Release/ && dotnet nuget push ${SOURCE} -s Artifactory -k ${1} && cd ../../../..
+dotnet nuget add source https://monetate.jfrog.io/artifactory/api/nuget/v3/dotnet-local -n Artifactory -u $USER -p $PW --store-password-in-clear-text
+
+cd cms-transforms-c-sharp/CmsTransformLibrary/bin/Release/ && dotnet nuget push ${SOURCE} -s Artifactory -k $1 && cd ../../../..
