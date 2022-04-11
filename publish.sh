a0eb873267b61cb61842c0d9c3123190791540a9
Tyler Weaver <Tyler.Weaver@kibocommerce.com>
2022-04-11 14:36:34 -0400

    tst

--- publish.sh
+++ publish.sh
@@ -5,8 +5,8 @@ VERSION_PREFIX="$(echo "$VERSION_PREFIX" | tr -d '\r\n')"
 SOURCE="CmsTransformLibrary.${VERSION_PREFIX}.nupkg"
 USER = $1
 PW = $2
-API_KEY = '$USER:$PW'
+API_KEY = '${USER}:${PW}'
 
-dotnet nuget add source https://monetate.jfrog.io/artifactory/api/nuget/v3/dotnet-local -n Artifactory -u $USER -p $PW --store-password-in-clear-text
+dotnet nuget add source https://monetate.jfrog.io/artifactory/api/nuget/v3/dotnet-local -n Artifactory -u ${USER} -p ${PW} --store-password-in-clear-text
 
-cd cms-transforms-c-sharp/CmsTransformLibrary/bin/Release/ && dotnet nuget push ${SOURCE} -s Artifactory -k $API_KEY && cd ../../../..
+cd cms-transforms-c-sharp/CmsTransformLibrary/bin/Release/ && dotnet nuget push ${SOURCE} -s Artifactory -k ${API_KEY} && cd ../../../..
